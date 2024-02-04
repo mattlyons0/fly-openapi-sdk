605d4d23715c0ed290bc92130db6f6530395cf03
Matt Lyons <matt@lyons.sh>
2024-02-04 00:27:28 -0500

    Update generation options

--- generate.sh
+++ generate.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i https://docs.machines.dev/swagger/doc.json  -g java -o /local/ --additional-properties apiPackage=io.fly.sdk --additional-properties artifactId=fly-sdk --additional-properties groupId=io.fly --additional-properties invokerPackage=io.fly.client --additional-properties modelPackage=io.fly.client.model --additional-properties library=resttemplate --additional-properties annotationLibrary=swagger2 --additional-properties generateClientAsBean=true --additional-properties useJakartaEe=true
+docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i https://docs.machines.dev/swagger/doc.json  -g java -o /local/ --additional-properties apiPackage=io.fly.sdk --additional-properties artifactId=fly-sdk --additional-properties groupId=io.fly --additional-properties invokerPackage=io.fly.client --additional-properties modelPackage=io.fly.client.model --additional-properties library=resttemplate --additional-properties annotationLibrary=swagger2 --additional-properties generateClientAsBean=true --additional-properties useJakartaEe=true --additional-properties hideGenerationTimestamp=true
 
 sudo chown -R $(whoami):$(whoami) .
