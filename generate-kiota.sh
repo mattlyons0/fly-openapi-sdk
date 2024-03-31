ff810d9064cb86d843ceffccba0aadd300c423be
Matt Lyons <matt@lyons.sh>
2024-03-30 23:43:07 -0400

    Remove OpenAPI Generator

--- generate-kiota.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker run --rm -v "/tmp/kiota:/local" mcr.microsoft.com/openapi/kiota \
-generate --language java -n io.fly.sdk -c FlySdkClient -o /local/ --openapi \
-https://docs.machines.dev/swagger/doc.json
--- generate.sh
+++ generate.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e
-
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i https://docs.machines.dev/swagger/doc.json  -g java -o /local/ --type-mappings integer=long --additional-properties apiPackage=io.fly.sdk --additional-properties artifactId=fly-sdk --additional-properties groupId=io.fly --additional-properties invokerPackage=io.fly.client --additional-properties modelPackage=io.fly.client.model --additional-properties library=resttemplate --additional-properties annotationLibrary=swagger2 --additional-properties generateClientAsBean=true --additional-properties useJakartaEe=true --additional-properties hideGenerationTimestamp=true --additional-properties artifactVersion=1.1.0
-
-sudo chown -R $(whoami):$(whoami) .
+docker run --rm mcr.microsoft.com/openapi/kiota \
+generate --language java -n io.fly.sdk -c FlySdkClient --openapi \
+https://docs.machines.dev/swagger/doc.json
